82a719033feff150993bfc89d5d1cd63f261b074
Irina Illustrova <illustrova@gmail.com>
2021-04-23 11:13:14 +0300

    scaling width

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 const debug = process.env.NODE_ENV !== "production";
 
 module.exports = {
-  
-  assetPrefix: !debug ? '/canvas-editor-experiment/' : '',
+  assetPrefix: !debug ? "/canvas-editor-experiment/" : "",
+};
