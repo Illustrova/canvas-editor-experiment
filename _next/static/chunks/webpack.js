6f7f563342e1e31c0a402a690970ba13e0c784dd
Irina Illustrova <illustrova@gmail.com>
2021-04-23 03:18:05 +0300

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "8dc8c07190059fb3b046";
+/******/ 	var hotCurrentHash = "243c69699caba59104f9";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
